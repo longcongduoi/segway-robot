acdf72a0df7ab531160c8010c010f7a8dce5b489
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2016-06-30 22:37:14 +0300

    Better motor control, start reducing Arduinoisms

--- segway-robot.ino
+++ segway-robot.ino
@@ -3,7 +3,7 @@
 
 // MPU parameters
 const int16_t MPU_OFFSETS[] = { 266, -239, 2528, 0, 50, 9, 13 };
-const int16_t ALPHA = 1;
+const int16_t ALPHA = 2;
 const int MPU_ADDR = 0x68;
 const mpuconfig MPU_CONF = {
     .disableTemp = true,
@@ -14,10 +14,6 @@ const mpuconfig MPU_CONF = {
     .enableInterrupt = true
 };
 
-// Motor pin configuration
-const int LEFT_A = 6, LEFT_B = 10,
-    RIGHT_A = 5, RIGHT_B = 9;
-
 // Motor PID controller params
 const int16_t MOTOR_P = 16, MOTOR_I = 0, MOTOR_D = 0;
 // Output limits
@@ -26,50 +22,78 @@ const int16_t MOTOR_MIN = -255, MOTOR_MAX = 255;
 // Runtime global variables
 volatile bool gMpuInterrupt = false;
 int16_t gPitch = 0, gMotorPwm = 0, gPitchSetPoint = 0, gMotorBias = 0;
+volatile uint8_t gLeftPwm = 0, gRightPwm = 0, gPortDclr = 0xFF, gPortDset = 0;
+
 pidparams gMotorPid;
 mpufilter gMpuFilter;
 
-void setupMotors() {
-    pinMode(LEFT_A, OUTPUT);
-    pinMode(LEFT_B, OUTPUT);
-    pinMode(RIGHT_A, OUTPUT);
-    pinMode(RIGHT_B, OUTPUT);
+ISR(TIMER1_OVF_vect) {
+    PORTD &= gPortDclr;
+    PORTD |= gPortDset;
+
+    OCR1A = gLeftPwm;
+    OCR1B = gRightPwm;
 }
 
-void updateMotors() {
-    updateMotor(LEFT_A, LEFT_B, gMotorPwm + gMotorBias);
-    updateMotor(RIGHT_A, RIGHT_B, gMotorPwm - gMotorBias);
-}
-
-void updateMotor(int pinA, int pinB, int pwm) {
-    if (pwm >= 0) {
-        digitalWrite(pinA, LOW);
-        analogWrite(pinB, pwm);
-    } else {
-        digitalWrite(pinA, HIGH);
-        analogWrite(pinB, 255 + pwm);
-    }
-}
-
-void mpuInterrupt() {
+ISR(INT0_vect) {
     gMpuInterrupt = true;
 }
 
+void setupMotors() {
+    // Set the motor pins as outputs
+    DDRB |= _BV(PB1) | _BV(PB2);
+    DDRD |= _BV(PD5) | _BV(PD6);
+    // Clear the motor bits
+    PORTD &= ~(_BV(PD5) | _BV(PD6));
+
+    // Set up the timer for fast PWM at 1 kHz with overflow interrupt
+    TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
+    TCCR1B = _BV(WGM12) | _BV(CS10) | _BV(CS11);
+    TIMSK1 = _BV(TOIE1);
+}
+
+void updateMotors() {
+    int pwmLeft = gMotorPwm + gMotorBias,
+        pwmRight = gMotorPwm - gMotorBias;
+
+    gPortDclr = 0xFF;
+    gPortDset = 0;
+
+    if (pwmLeft >= 0) {
+        gPortDclr &= ~_BV(PD6);
+    } else {
+        gPortDset |= _BV(PD6);
+        pwmLeft += 255;
+    }
+
+    if (pwmRight >= 0) {
+        gPortDclr &= ~_BV(PD5);
+    } else {
+        gPortDset |= _BV(PD5);
+        pwmRight += 255;
+    }
+
+    gLeftPwm = pwmLeft;
+    gRightPwm = pwmRight;
+}
+
 void setup() {
-    setupMotors();
-
-    Serial.begin(115200);
-
     Wire.begin();
     Wire.setClock(400000L);
 
     mpuSetup(MPU_ADDR, &MPU_CONF);
     mpuSetupFilter(&MPU_CONF, &gMpuFilter, ALPHA);
-    attachInterrupt(digitalPinToInterrupt(2), mpuInterrupt, RISING);
+
+    // Setup the MPU pin change interrupt
+    // Detect rising edges on INT0 pin
+    EICRA = _BV(ISC01) | _BV(ISC00);
+    EIMSK = _BV(INT0);
     mpuReadIntStatus(MPU_ADDR);
 
     pidInit(&gMotorPid, &gPitchSetPoint, &gPitch, &gMotorPwm,
         MOTOR_P, MOTOR_I, MOTOR_D, MOTOR_MIN, MOTOR_MAX);
+
+    setupMotors();
 }
 
 void loop() {
@@ -85,8 +109,6 @@ void loop() {
 
         pidCompute(&gMotorPid);
         updateMotors();
-
-        Serial.println(gMotorPwm);
     }
 }
 
